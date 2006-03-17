f28e8728688574f088d4bc510cf18f8d11ad03bb
zerg <zerg>
2006-03-17 12:17:58 +0000

    - add

---
+++ updater.cc
@@ -0,0 +1,33 @@
+
+#include "updater.hh"
+#include "connection.hh"
+#include "browser.hh"
+
+Updater::Updater():
+	QObject(),
+	timer_(new QTimer(this))
+{
+	timer_->setInterval(500);
+	connect(timer_,SIGNAL(timeout()),this,SLOT(doUpdate()));
+}
+
+void Updater::start()
+{
+	timer_->start();
+}
+
+void Updater::stop()
+{
+	timer_->stop();
+}
+
+void Updater::doUpdate()
+{
+	getDocument(getDocParser,"(alterator-request action \"re-get\")");
+}
+
+
+void Updater::doRetry()
+{
+	getDocument(getDocParser);
+}
---
+++ updater.hh
@@ -0,0 +1,24 @@
+#ifndef QT_BROWSER_UPDATER_HH
+#define QT_BROWSER_UPDATER_HH
+
+#include <QObject>
+#include <QTimer>
+
+#include "browser.hh"
+
+class Updater: public QObject
+{
+Q_OBJECT
+	public:
+		Updater();
+	public slots:
+		void doUpdate();
+		void doRetry();
+		void start();
+		void stop();
+	private:
+		QTimer *timer_;
+};
+
+
+#endif
