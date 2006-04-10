5170c130808ecb4af21d48e75174ceb143cecd8a
zerg <zerg>
2006-04-10 16:51:17 +0000

    - start pixmaps cache

---
+++ a_pixmaps.cc
@@ -0,0 +1,85 @@
+#include <QPixmapCache>
+#include <QApplication>
+#include <QPainter>
+#include <QRect>
+#include <QStyleOptionButton>
+
+#include "global.hh"
+#include "a_pixmaps.hh"
+
+APixmaps::APixmaps()
+{
+    standard["theme:information"] = QStyle::SP_MessageBoxInformation;
+    standard["theme:warning"] = QStyle::SP_MessageBoxWarning;
+    standard["theme:critical"] = QStyle::SP_MessageBoxCritical;
+    standard["theme:question"] = QStyle::SP_MessageBoxQuestion;
+    standard["theme:desktop"] = QStyle::SP_DesktopIcon;
+    standard["theme:trash"] = QStyle::SP_TrashIcon;
+    standard["theme:computer"] = QStyle::SP_ComputerIcon;
+    standard["theme:fd"] = QStyle::SP_DriveFDIcon;
+    standard["theme:hd"] = QStyle::SP_DriveHDIcon;
+    standard["theme:cd"] = QStyle::SP_DriveCDIcon;
+    standard["theme:dvd"] = QStyle::SP_DriveDVDIcon;
+    standard["theme:net"] = QStyle::SP_DriveNetIcon;
+    standard["theme:dir"] = QStyle::SP_DirOpenIcon;
+    standard["theme:file"] = QStyle::SP_FileIcon;
+    generated["theme:checkbox_checked"] = qMakePair(QStyle::PE_IndicatorCheckBox, (int)QStyle::State_On);
+    generated["theme:checkbox_unchecked"] = qMakePair(QStyle::PE_IndicatorCheckBox, (int)QStyle::State_Off);
+}
+
+APixmaps::~APixmaps() {}
+
+QPixmap APixmaps::get(const QString &id)
+{
+    QPixmap pixmap;
+    if( !QPixmapCache::find(id, pixmap) )
+    {
+	bool is_generated = false;
+	if( id.startsWith("theme:") )
+	{
+	    if( standard.contains(id) )
+		pixmap = QApplication::style()->standardPixmap(standard[id]);
+	    else if( generated.contains(id) )
+	    {
+		is_generated = true;
+		pixmap = generate(id);
+	    }
+	}
+	else
+	    pixmap = QPixmap( IMAGES_PATH + id );
+
+	if( pixmap.width() <= 0 || pixmap.height() <= 0 )
+	    pixmap = QApplication::style()->standardPixmap(standard["theme:question"]);
+	else if( (pixmap.width() <= 48 && pixmap.height() <= 48)
+		|| is_generated )
+	    QPixmapCache::insert(id, pixmap);
+    }
+    return pixmap;
+}
+
+QPixmap APixmaps::generate(const QString &id)
+{
+    QPixmap pix;
+    if( generated.contains(id) )
+    {
+	QPair<QStyle::PrimitiveElement, int> pair(generated[id]);
+	switch( pair.first )
+	{
+	    case QStyle::PE_IndicatorCheckBox:
+	    {
+		QStyle::StateFlag opt = (QStyle::StateFlag)pair.second;
+    		QStyleOptionButton b;
+		QRect r = QApplication::style()->subElementRect(QStyle::SE_CheckBoxIndicator,&b);
+		pix = QPixmap(r.width(),r.height());
+		QPainter paint(&pix);
+		b.state |= opt;
+		b.rect = QRect(0,0,r.width(),r.height());
+		QApplication::style()->drawPrimitive(QStyle::PE_IndicatorCheckBox,&b,&paint);
+		break;
+	    }
+	    default:
+		break;
+	}
+    }
+    return pix;
+}
---
+++ a_pixmaps.hh
@@ -0,0 +1,24 @@
+#ifndef QTBROWSER_APIXMAPS_HH
+#define QTBROWSER_APIXMAPS_HH
+
+#include <QString>
+#include <QPixmap>
+#include <QMap>
+#include <QStyle>
+
+class APixmaps
+{
+public:
+    APixmaps();
+    ~APixmaps();
+
+    QPixmap get(const QString &id);
+
+private:
+    QMap<QString, QStyle::StandardPixmap> standard;
+    QMap<QString, QPair<QStyle::PrimitiveElement, int> > generated;
+
+    QPixmap generate(const QString &id);
+};
+
+#endif
