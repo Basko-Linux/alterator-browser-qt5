8b897e4095c698e1bbc1bc3cfae65f772487d739
zerg <zerg>
2006-04-28 12:29:24 +0000

    - fix border

--- hacks.cc
+++ hacks.cc
@@ -6,8 +6,8 @@ void widget_corners_round(QWidget *widget)
 {
 	QPainter painter(widget);
 	
-	painter.drawLine(1,1,1,widget->height()-1);
-	painter.drawLine(1,1,widget->width()-1,1);
-	painter.drawLine(widget->width()-1,1,widget->width()-1,widget->height()-1);
-	painter.drawLine(1,widget->height()-1,widget->width()-1,widget->height()-1);
+	painter.drawLine(1,1,1,widget->height()-2);
+	painter.drawLine(1,1,widget->width()-2,1);
+	painter.drawLine(widget->width()-2,1,widget->width()-2,widget->height()-2);
+	painter.drawLine(1,widget->height()-2,widget->width()-2,widget->height()-2);
 }
