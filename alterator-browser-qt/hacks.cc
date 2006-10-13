fa441cf692e37588db175eb296a8ac9592663836
Sergey V Turchin <zerg@altlinux.org>
2006-10-13 16:49:00 +0400

    - round corners

--- alterator-browser-qt/hacks.cc
+++ alterator-browser-qt/hacks.cc
@@ -1,15 +1,109 @@
+// Copyright (c) 2004 Stanislav Ievlev <inger@altlinux.org>
+// Copyright (c) 2004 Sergey V Turchin <zerg@altlinux.org>
+// widget_corners_round() Copyright (c) 2003, 2004 David Johnson <david@usermode.org>
+
 #include "hacks.hh"
 
 #include <QPainter>
+#include <QColorGroup>
 
 void widget_corners_round(QWidget *widget)
 {
-	QPainter painter(widget);
-	
-	painter.drawLine(1,1,1,widget->height()-2);
-	painter.drawLine(1,1,widget->width()-2,1);
-	painter.drawLine(widget->width()-2,1,widget->width()-2,widget->height()-2);
-	painter.drawLine(1,widget->height()-2,widget->width()-2,widget->height()-2);
+    int framesize_ = 8;
+
+    QPainter painter(widget);
+    QPalette palette = widget->palette();
+
+    QBrush bg_brush = palette.brush( QPalette::Background );
+    QColor dark_color = palette.dark().color();
+    // top
+    QRect frame(0, 0, widget->width(), framesize_);
+    painter.fillRect(frame, bg_brush);
+    // left
+    frame.setRect(0, 0, framesize_, widget->height());
+    painter.fillRect(frame, bg_brush);
+    // bottom
+    frame.setRect(0, widget->height() - (framesize_*2), widget->width(),  (framesize_*2));
+    painter.fillRect(frame, bg_brush);
+    // right
+    frame.setRect(widget->width()-framesize_, 0, framesize_, widget->height());
+    painter.fillRect(frame, bg_brush);
+    // outline the frame
+    painter.setPen( dark_color );
+    painter.setPen(QPen(QColor("black")));
+    frame = widget->rect();
+
+    painter.drawRect(frame);
+    // set the inner frame
+    painter.setPen( dark_color );
+    frame.setRect(frame.x() + framesize_-1, frame.y() + framesize_-1,
+	frame.width() - framesize_*2 +2,
+	frame.height() - (framesize_*2) +2);
+
+    // local temp right and bottom
+    int r(widget->width());
+    int b(widget->height());
+    // draw the inner frame with rounded corners
+    painter.drawRoundRect(frame, 1, 1);
+    // choose a color from scheme
+	// painter.setPen( dark_color );
+    painter.setPen(QPen(QColor("black")));
+
+    // Draw edge of top-left corner inside the area removed by the mask.
+    painter.drawPoint(4, 1);
+    painter.drawPoint(3, 1);
+    painter.drawPoint(2, 2);
+    painter.drawPoint(1, 3);
+    painter.drawPoint(1, 4);
+
+    // Draw edge of top-right corner inside the area removed by the mask.
+    painter.drawPoint(r - 5, 1);
+    painter.drawPoint(r - 4, 1);
+    painter.drawPoint(r - 3, 2);
+    painter.drawPoint(r - 2, 3);
+    painter.drawPoint(r - 2, 4);
+
+    // Draw edge of bottom-left corner inside the area removed by the mask.
+    painter.drawPoint(1, b - 5);
+    painter.drawPoint(1, b - 4);
+    painter.drawPoint(2, b - 3);
+    painter.drawPoint(3, b - 2);
+    painter.drawPoint(4, b - 2);
+
+    // Draw edge of bottom-right corner inside the area removed by the mask.
+    painter.drawPoint(r - 2, b - 5);
+    painter.drawPoint(r - 2, b - 4);
+    painter.drawPoint(r - 3, b - 3);
+    painter.drawPoint(r - 4, b - 2);
+    painter.drawPoint(r - 5, b - 2);
+
+    // mask off which corners we dont want
+    QRegion mask;
+
+    mask=QRegion(widget->rect());
+
+    // Remove top-left corner.
+    mask -= QRegion(0, 0, 5, 1);
+    mask -= QRegion(0, 1, 3, 1);
+    mask -= QRegion(0, 2, 2, 1);
+    mask -= QRegion(0, 3, 1, 2);
+	// Remove top-right corner.
+    mask -= QRegion(r - 5, 0, 5, 1);
+    mask -= QRegion(r - 3, 1, 3, 1);
+    mask -= QRegion(r - 2, 2, 2, 1);
+    mask -= QRegion(r - 1, 3, 1, 2);
+	// Remove bottom-left corner.
+    mask -= QRegion(0, b - 5, 1, 3);
+    mask -= QRegion(0, b - 3, 2, 1);
+    mask -= QRegion(0, b - 2, 3, 1);
+    mask -= QRegion(0, b - 1, 5, 1);
+	// Remove bottom-right corner.
+    mask -= QRegion(r - 5, b - 1, 5, 1);
+    mask -= QRegion(r - 3, b - 2, 3, 1);
+    mask -= QRegion(r - 2, b - 3, 2, 1);
+    mask -= QRegion(r - 1, b - 5, 1, 2);
+
+    widget->setMask(mask);
 }
 
 QListWidget2::QListWidget2(QWidget *parent):
