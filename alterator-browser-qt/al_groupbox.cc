8b27c1cc07a10b324a81a2f0fa13f72251ccd9e9
Sergey V Turchin <zerg@altlinux.org>
2006-11-29 17:30:32 +0300

    - add

---
+++ alterator-browser-qt/al_groupbox.cc
@@ -0,0 +1,41 @@
+
+#include "al_groupbox.hh"
+
+alGroupBox::alGroupBox(const QString& id,const QString& parent, Qt::Orientation orientation, const QString& checkable):
+	alWidgetPre<QGroupBox>((orientation==Qt::Vertical)?VGroupBox:HGroupBox,id,parent)
+{
+    wnd_->setCheckable("true" == checkable);
+    QBoxLayout *l;
+    if( orientation == Qt::Horizontal )
+	l = new QHBoxLayout(getViewWidget());
+    else
+	l = new QVBoxLayout(getViewWidget());
+    l->setSpacing(5);
+    l->setMargin(5);
+}
+
+void alGroupBox::setAttr(const QString& name,const QString& value)
+{
+	if ("title" == name)
+		wnd_->setTitle(value);
+	else if ("state" == name)
+		wnd_->setChecked("true" ==  value);
+	else
+		alWidget::setAttr(name,value);
+}
+
+void alGroupBox::registerEvent(const QString& name)
+{
+	if ("toggled" == name)
+		connect(wnd_,SIGNAL( toggled(bool) ),SLOT(onToggle(bool)));
+}
+
+
+QString alGroupBox::postData() const
+{
+	if (wnd_->isCheckable())
+		return QString(" (state . ")+ (wnd_->isChecked()?"#t":"#f") +" )";
+	else
+		return "";
+}
+
---
+++ alterator-browser-qt/al_groupbox.hh
@@ -0,0 +1,29 @@
+
+#include <QGroupBox>
+
+#include "al_widget.hh"
+
+class alGroupBox: public alWidgetPre<QGroupBox>
+{
+public:
+	alGroupBox(const QString& id,const QString& parent, Qt::Orientation orientation, const QString& checkable);
+	void setAttr(const QString& name,const QString& value);
+	void registerEvent(const QString&);
+	QString postData() const ;
+};
+
+class alVGroupBox: public alGroupBox
+{
+public:
+	alVGroupBox(const QString& id,const QString& parent, const QString& checkable):
+		alGroupBox(id,parent,Qt::Vertical, checkable)
+	{}
+};
+
+class alHGroupBox: public alGroupBox
+{
+public:
+	alHGroupBox(const QString& id,const QString& parent, const QString& checkable):
+		alGroupBox(id,parent,Qt::Horizontal, checkable)
+	{}
+};
