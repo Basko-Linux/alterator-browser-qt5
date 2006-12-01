430d45b451ace5f01441a0a363bcaa9542674503
Sergey V Turchin <zerg@altlinux.org>
2006-12-01 10:55:48 +0300

    - remove set alTabPage::widget-name

--- alterator-browser-qt/widgets.cc
+++ alterator-browser-qt/widgets.cc
@@ -395,9 +395,6 @@ void alTabPage::setAttr(const QString& name,const QString& value)
 {
 	if ("text" == name)
 		static_cast<QTabWidget*>(tabbox_->getWidget())->setTabText(idx_,value);
-	else if (("widget-name" == name) &&
-		    (tabbox_ && tabbox_->current_ == value))
-		static_cast<QTabWidget*>(tabbox_->getWidget())->setCurrentIndex(idx_);
 	else
 		alWidget::setAttr(name,value);
 }
