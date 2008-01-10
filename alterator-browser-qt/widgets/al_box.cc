f890e67707747a722c97fc23e9832f7a726faefc
Sergey V Turchin <zerg@altlinux.org>
2008-01-10 17:56:30 +0300

    - typo

--- alterator-browser-qt/widgets/al_box.cc
+++ alterator-browser-qt/widgets/al_box.cc
@@ -4,7 +4,7 @@
 alBox::alBox(const AlteratorRequestActionAttrs &attr, const QString& id,const QString& parent, Qt::Orientation orientation):
 		alWidgetPre<QWidget>(attr,(orientation==Qt::Vertical)?WVBox:WHBox,id,parent)
 {
-    qDebug("vbox, hbox, box widgets are depricated. Use gridbox instead.");
+    qDebug("vbox, hbox, box widgets are deprecated. Use gridbox instead.");
 
     QBoxLayout *l;
     Qt::Orientation o = Utils::fixOrientation(orientation, Qt::Vertical);
