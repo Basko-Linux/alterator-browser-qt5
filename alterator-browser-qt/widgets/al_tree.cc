eac9cd349d22276bb35e34ee418194e8bf490c54
Sergey V Turchin <zerg@altlinux.org>
2008-03-04 12:57:04 +0300

    - improve look

--- alterator-browser-qt/widgets/al_tree.cc
+++ alterator-browser-qt/widgets/al_tree.cc
@@ -10,6 +10,9 @@ alTree::alTree(const AlteratorRequestActionAttrs &attr, const QString& id,const 
     expanded_ = false;
     //setings to be compatible with QListView
     wnd_->setAlternatingRowColors(true);
+    wnd_->setAllColumnsShowFocus(true);
+    wnd_->setUniformRowHeights(true);
+    wnd_->setSelectionBehavior(QAbstractItemView::SelectRows);
     int col_count = columns.toInt();
     if( col_count <1 ) col_count = 1;
     wnd_->setColumnCount(col_count);
