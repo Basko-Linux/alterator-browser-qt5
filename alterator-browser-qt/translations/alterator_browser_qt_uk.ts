0a15cf12fe7973905f2e58d9c2840c0ed95df56d
Andrey Cherepanov <cas@altlinux.org>
2011-01-11 16:48:08 +0300

    Update Ukrainian translations (thanks Roman Savochenko)

--- alterator-browser-qt/translations/alterator_browser_qt_uk.ts
+++ alterator-browser-qt/translations/alterator_browser_qt_uk.ts
@@ -1,29 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="uk_UA">
+<defaultcodec></defaultcodec>
 <context>
     <name>ACenterFace</name>
     <message>
+        <source>Owerview</source>
+        <translation type="obsolete">Огляд</translation>
+    </message>
+    <message>
         <location filename="widgets/al_center_face.cc" line="228"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Довідка</translation>
     </message>
     <message>
         <location filename="widgets/al_center_face.cc" line="237"/>
         <source>Expert mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Режим експерту</translation>
+    </message>
+    <message>
+        <source>Contents</source>
+        <translation type="obsolete">Зміст</translation>
     </message>
     <message>
         <location filename="widgets/al_center_face.cc" line="329"/>
         <location filename="widgets/al_center_face.cc" line="591"/>
         <source>Main</source>
-        <translation type="unfinished"></translation>
+        <translation>Головна</translation>
     </message>
     <message>
         <location filename="widgets/al_center_face.cc" line="220"/>
         <location filename="widgets/al_center_face.cc" line="335"/>
         <source>Module</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль</translation>
     </message>
 </context>
 <context>
@@ -32,12 +41,12 @@
         <location filename="widgets/al_wizard_face.cc" line="298"/>
         <location filename="widgets/al_wizard_face.cc" line="923"/>
         <source>Show steps list</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати перелік кроків</translation>
     </message>
     <message>
         <location filename="widgets/al_wizard_face.cc" line="921"/>
         <source>Hide steps list</source>
-        <translation type="unfinished"></translation>
+        <translation>Приховати перелік кроків</translation>
     </message>
 </context>
 <context>
@@ -46,22 +55,29 @@
         <location filename="../browser.cc" line="234"/>
         <location filename="../browser.cc" line="241"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Вихід</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="234"/>
         <source>Exit Alterator?</source>
-        <translation type="unfinished"></translation>
+        <translation>Завершити Alterator?</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="256"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Про програму</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="256"/>
         <source>Alterator Browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Alterator Browser</translation>
+    </message>
+</context>
+<context>
+    <name>HelpBrowserUI</name>
+    <message>
+        <source>Help</source>
+        <translation type="obsolete">Довідка</translation>
     </message>
 </context>
 <context>
@@ -69,48 +85,78 @@
     <message>
         <location filename="../help_browser.cc" line="35"/>
         <source>Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Сесія</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="17"/>
         <location filename="../help_browser.cc" line="36"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Довідка</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="39"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Вихід</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="40"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Про програму</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="41"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Про Qt</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="73"/>
         <source>No help available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Довідка недоступна.</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="101"/>
         <source>HELP_BROWSER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Змінна HELP_BROWSER порожня</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="104"/>
         <source>BROWSER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Змінна BROWSER порожня</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="111"/>
         <source>MAILER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Змінна MAILER порожня</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Quit</source>
+        <translation type="obsolete">Вихід</translation>
+    </message>
+    <message>
+        <source>Exit Alterator?</source>
+        <translation type="obsolete">Завершити Alterator?</translation>
+    </message>
+    <message>
+        <source>Alterator Browser</source>
+        <translation type="obsolete">Alterator Browser</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="obsolete">Про програму</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>About</source>
+        <translation type="obsolete">Про програму</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="obsolete">Помилка</translation>
     </message>
 </context>
 </TS>
