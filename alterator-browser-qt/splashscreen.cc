5051d59aa49959e39ae7dfdd781b30034ed3ff67
Sergey V Turchin <zerg@altlinux.org>
2011-07-13 16:09:28 +0400

    - cleanup

--- alterator-browser-qt/splashscreen.cc
+++ alterator-browser-qt/splashscreen.cc
@@ -15,21 +15,12 @@ SplashScreen::SplashScreen(QWidget *parent):
 	l->setMargin(0);
 
     label = new QLineEdit(this);
-#if 0
-    label->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-#endif
     label->setAlignment(Qt::AlignCenter);
     label->setFrame(false);
     label->setReadOnly(true);
     label->setDragEnabled(false);
     lay->addWidget(label);
 
-#if 0
-    setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-    setFrameStyle(QFrame::StyledPanel| QFrame::Sunken);
-    move(parent->geometry().center() - geometry().center());
-    raise();
-#endif
     show();
 }
 
@@ -42,10 +33,4 @@ void SplashScreen::setText(const QString &txt)
     label->setText(txt);
     QFontMetrics m(label->font());
     setMinimumWidth(m.boundingRect(txt).width() + 25);
-#if 0
-    QRect pg(parentWidget()->geometry());
-    int w = width();
-    int h = height();
-    setGeometry((pg.width()-w)/2, (pg.height()-h)/2,w ,h);
-#endif
 }
