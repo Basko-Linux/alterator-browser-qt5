f4b8c911804083129511d54cc89aee2d5f1ce8de
Sergey V Turchin <zerg@altlinux.org>
2013-03-25 18:14:52 +0400

    - update Kazakh translation

--- alterator-browser-qt/translations/alterator_browser_qt_kk.ts
+++ alterator-browser-qt/translations/alterator_browser_qt_kk.ts
@@ -1,39 +1,39 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="kk_KZ" sourcelanguage="en_US">
 <context>
     <name>ACenterFace</name>
     <message>
         <location filename="../widgets/al_center_face.cc" line="240"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Көмек</translation>
     </message>
     <message>
         <location filename="../widgets/al_center_face.cc" line="249"/>
         <source>Expert mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Сарапшы режимі</translation>
     </message>
     <message>
         <location filename="../widgets/al_center_face.cc" line="257"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Шығу</translation>
     </message>
     <message>
         <location filename="../widgets/al_center_face.cc" line="350"/>
         <location filename="../widgets/al_center_face.cc" line="658"/>
         <source>Main</source>
-        <translation type="unfinished"></translation>
+        <translation>Басты</translation>
     </message>
     <message>
         <location filename="../widgets/al_center_face.cc" line="643"/>
         <source>External module &quot;%1&quot; running...</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; сыртқы модулі орындалып тұр...</translation>
     </message>
     <message>
         <location filename="../widgets/al_center_face.cc" line="232"/>
         <location filename="../widgets/al_center_face.cc" line="357"/>
         <source>Module</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль</translation>
     </message>
 </context>
 <context>
@@ -42,12 +42,12 @@
         <location filename="../widgets/al_wizard_face.cc" line="300"/>
         <location filename="../widgets/al_wizard_face.cc" line="925"/>
         <source>Show steps list</source>
-        <translation type="unfinished"></translation>
+        <translation>Қадамдар тізімін көрсету</translation>
     </message>
     <message>
         <location filename="../widgets/al_wizard_face.cc" line="923"/>
         <source>Hide steps list</source>
-        <translation type="unfinished"></translation>
+        <translation>Қадамдар тізімін жасыру</translation>
     </message>
 </context>
 <context>
@@ -56,22 +56,22 @@
         <location filename="../browser.cc" line="244"/>
         <location filename="../browser.cc" line="251"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Шығу</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="244"/>
         <source>Exit Alterator?</source>
-        <translation type="unfinished"></translation>
+        <translation>Alterator-дан шығу керек пе?</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="266"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Осы туралы</translation>
     </message>
     <message>
         <location filename="../browser.cc" line="266"/>
         <source>Alterator Browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Alterator Browser</translation>
     </message>
 </context>
 <context>
@@ -79,48 +79,48 @@
     <message>
         <location filename="../help_browser.cc" line="35"/>
         <source>Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Сессия</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="17"/>
         <location filename="../help_browser.cc" line="36"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Көмек</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="39"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Шығу</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="40"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Осы туралы</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="41"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt туралы</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="73"/>
         <source>No help available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Көмек қолжетерсіз.</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="101"/>
         <source>HELP_BROWSER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>HELP_BROWSER қоршам айнымалысы бос</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="104"/>
         <source>BROWSER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>BROWSER қоршам айнымалысы бос</translation>
     </message>
     <message>
         <location filename="../help_browser.cc" line="111"/>
         <source>MAILER environment variable is empty</source>
-        <translation type="unfinished"></translation>
+        <translation>MAILER қоршам айнымалысы бос</translation>
     </message>
 </context>
 </TS>
