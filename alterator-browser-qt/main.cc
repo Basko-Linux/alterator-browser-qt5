2f0ea23d7c8b4b44ccc0067f9d9031db029c7a37
Sergey V Turchin <zerg@altlinux.org>
2017-04-20 13:09:13 +0300

    - add virtual keyboard support

--- alterator-browser-qt/main.cc
+++ alterator-browser-qt/main.cc
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <csignal>
 
+#include <libudev.h>
+
 #include <QApplication>
 #include "browser.hh"
 #include "enums.hh"
@@ -59,6 +61,40 @@ int main(int argc,char **argv)
 	QFile pidfile(pidfile_path);
 	if( pidfile.open(QIODevice::WriteOnly|QIODevice::Truncate) )
 	{
+	    int num_hw_keyboards = 0;
+	    {
+	        struct udev *udev;
+	        struct udev_enumerate *enumerate;
+	        struct udev_list_entry *devices, *dev_list_entry;
+	        struct udev_device *dev;
+
+		udev = udev_new();
+		if( udev ) {
+		    enumerate = udev_enumerate_new(udev);
+		    udev_enumerate_add_match_property(enumerate, "ID_INPUT_KEYBOARD", "1");
+		    udev_enumerate_scan_devices(enumerate);
+		    devices = udev_enumerate_get_list_entry(enumerate);
+		    udev_list_entry_foreach(dev_list_entry, devices) {
+			const char *path, *devnode;
+			path = udev_list_entry_get_name(dev_list_entry);
+			dev = udev_device_new_from_syspath(udev, path);
+			devnode = udev_device_get_devnode(dev);
+			if (devnode) {
+			    num_hw_keyboards++;
+			}
+			udev_device_unref(dev);
+		    }
+		    udev_enumerate_unref(enumerate);
+		    udev_unref(udev);
+		} else {
+		    qWarning("Can't create udev handle.");
+		}
+	    }
+	    if( num_hw_keyboards <= 0 ) {
+		// try to use virtual keyboard
+		qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
+	    }
+
 	    QString write_line = QString("%1").arg(getpid());
 	    pidfile.write(write_line.toLocal8Bit());
 	    pidfile.close();
